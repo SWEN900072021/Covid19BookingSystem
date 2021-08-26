72a7f5a2ebdb4cb11e6c0ccc40e309ba51f1beef
Jay Parikh <jparikh@student.unimelb.edu.au>
2021-08-26 19:30:51 +1000

    Updated SQL create statements after review

--- scripts/postgres_setup.sql
+++ scripts/postgres_setup.sql
@@ -5,71 +5,71 @@ CREATE TYPE account_type AS ENUM ('VR', 'HCP', 'ADMIN');
 CREATE TYPE hcp_type AS ENUM ('CLINIC', 'HOSPITAL', 'GP', 'POPUP', 'OTHER');
 CREATE TYPE outcome AS ENUM ('PASS', 'FAIL');
 
-CREATE TABLE IF NOT EXISTS accounts (
-    account_id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS account (
+    id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(50) NOT NULL,
     account_type account_type NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS healthcare_providers (
-    hcp_id INT NOT NULL PRIMARY KEY,
-    account_id INT NOT NULL UNIQUE,
-    hcp_name VARCHAR(50) NOT NULL,
-    hcp_type hcp_type NOT NULL,
+CREATE TABLE IF NOT EXISTS healthcare_provider (
+    id SERIAL PRIMARY KEY,
+    organisational_id INT NOT NULL,
+    account INT UNIQUE,
+    hcp_name VARCHAR(50),
+    hcp_type hcp_type,
     postcode INT,
-    FOREIGN KEY (account_id)
-        REFERENCES accounts (account_id)
+    FOREIGN KEY (account)
+        REFERENCES account (id)
 );
 
-CREATE TABLE IF NOT EXISTS vaccine_recipients (
-    vr_id SERIAL PRIMARY KEY,
-    account_id INT NOT NULL UNIQUE,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    address VARCHAR(50),
+CREATE TABLE IF NOT EXISTS vaccine_recipient (
+    id SERIAL PRIMARY KEY,
+    account INT UNIQUE,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    address VARCHAR(100),
     date_of_birth DATE NOT NULL,
-    gender gender NOT NULL,
-    phone_number VARCHAR(20) NOT NULL,
-    email_address VARCHAR(50) NOT NULL,
-    vaccination_status vaccine_status NOT NULL,
-    vaccination_type vaccine_type NOT NULL,
-    FOREIGN KEY (account_id)
-        REFERENCES accounts (account_id)
+    gender gender,
+    phone_number VARCHAR(20),
+    email_address VARCHAR(50),
+    vaccination_status vaccine_status,
+    vaccination_type vaccine_type,
+    FOREIGN KEY (account)
+        REFERENCES account (id)
 );
 
-CREATE TABLE IF NOT EXISTS vaccine_certificates (
-    cert_id SERIAL PRIMARY KEY,
-    vr_id INT UNIQUE NOT NULL,
-    hcp_id INT NOT NULL,
-    vaccination_type vaccine_type NOT NULL,
+CREATE TABLE IF NOT EXISTS vaccine_certificate (
+    id SERIAL PRIMARY KEY,
+    vaccine_recipient INT UNIQUE NOT NULL,
+    healthcare_provider INT NOT NULL,
+    vaccination_type vaccine_type,
     date_issued DATE NOT NULL,
-    FOREIGN KEY (vr_id)
-        REFERENCES vaccine_recipients (vr_id),
-    FOREIGN KEY (hcp_id)
-        REFERENCES healthcare_providers (hcp_id)
+    FOREIGN KEY (vaccine_recipient)
+        REFERENCES vaccine_recipient (id),
+    FOREIGN KEY (healthcare_provider)
+        REFERENCES healthcare_provider (id)
 );
 
-CREATE TABLE IF NOT EXISTS questionnaires (
-    questionnaire_id SERIAL PRIMARY KEY,
-    date_taken DATE NOT NULL,
-    outcome outcome NOT NULL,
-    answers VARCHAR(200)
+CREATE TABLE IF NOT EXISTS questionnaire (
+    id SERIAL PRIMARY KEY,
+    date_taken DATE,
+    outcome outcome
 );
 
-CREATE TABLE IF NOT EXISTS timeslots (
-    timeslot_id SERIAL PRIMARY KEY,
-    vr_id INT,
-    hcp_id INT NOT NULL,
-    questionnaire_id INT,
-    vaccination_type vaccine_type NOT NULL,
+CREATE TABLE IF NOT EXISTS timeslot (
+    id SERIAL PRIMARY KEY,
+    vaccine_recipient INT,
+    healthcare_provider INT NOT NULL,
+    questionnaire INT,
+    vaccination_type vaccine_type,
     date_time TIMESTAMP NOT NULL,
-    duration INT NOT NULL,
-    location VARCHAR(50) NOT NULL,
-    FOREIGN KEY (vr_id)
-        REFERENCES vaccine_recipients (vr_id),
-    FOREIGN KEY (hcp_id)
-        REFERENCES healthcare_providers (hcp_id),
-    FOREIGN KEY (questionnaire_id)
-        REFERENCES questionnaires (questionnaire_id)
+    duration INT,
+    location VARCHAR(100),
+    FOREIGN KEY (vaccine_recipient)
+        REFERENCES vaccine_recipient (id),
+    FOREIGN KEY (healthcare_provider)
+        REFERENCES healthcare_provider (id),
+    FOREIGN KEY (questionnaire)
+        REFERENCES questionnaire (id)
 );
