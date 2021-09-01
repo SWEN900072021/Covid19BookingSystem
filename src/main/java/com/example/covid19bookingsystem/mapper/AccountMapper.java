f8ae7c5fc8fcae7a588277778b1857977a7a0f09
Jay Parikh <jparikh@student.unimelb.edu.au>
2021-09-01 22:03:43 +1000

    Added enum class for all custom postgres types

--- src/main/java/com/example/covid19bookingsystem/mapper/AccountMapper.java
+++ src/main/java/com/example/covid19bookingsystem/mapper/AccountMapper.java
@@ -1,24 +1,22 @@
 package com.example.covid19bookingsystem.mapper;
 
 import com.example.covid19bookingsystem.datasource.DBConnection;
+import com.example.covid19bookingsystem.utils.EnumUtils;
 
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
 
 public class AccountMapper {
     public void insert(String username, String password) {
-        String sql = "INSERT INTO account (username, password, account_type) VALUES (?, ?, ?::account_type);";
+        String sql = "INSERT INTO account (username, password, account_type) VALUES (?, ?, ?);";
         PreparedStatement findStatement = null;
-
-        String accountType = "VR";
 
         try {
             findStatement = DBConnection.getDbConnection().prepareStatement(sql);
             findStatement.setString(1, username);
             findStatement.setString(2, password);
-            findStatement.setString(3, accountType);
+            findStatement.setObject(3, EnumUtils.AccountType.HCP, Types.OTHER);
             findStatement.execute();
         } catch (SQLException e) {
             System.out.println("Account Mapper Error: " + e.getMessage());
